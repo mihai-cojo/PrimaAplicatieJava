1e7c8250d9fd0d564a2ec042f7393e20a485c524
mihai-cojo <mihai.cojokeru@gmail.com>
2020-12-27 19:51:43 +0200

    Adaugare comentarii pe mai multe linii

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -3,7 +3,15 @@ package ro.java.ctrln;
 public class Main {
     //JAR - Java Archive
     //JAVA_HOME
-    public static void main(String[] args) {
+    /*
+    Aici este un exemplu
+    de comentariu
+    pe mai multe linii
+     */
+    public static void main(/*args este parametrul metodei main*/String[] args) {
+        /* Inainte de comentariu nu se poate pune text
+        Textul de aici face parte din comentariul multi-linie
+         */
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
 }
