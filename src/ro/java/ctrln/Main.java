89be6d04a0c1fa4d1fb136a778fef32a53aba117
mihai-cojo <mihai.cojokeru@gmail.com>
2020-12-27 19:36:32 +0200

    Adaugare comentarii pe o singura linie

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,9 +1,11 @@
 package ro.java.ctrln;
 
 public class Main {
-    //JAR - Java Archive
-    //JAVA_HOME
+    
+    // Aceasta este metoda main, care porneste aplicatia Java
     public static void main(String[] args) {
+        // Inainte de comentariu nu se poate pune text
+        // Aici vrem sa afisam pe consola Intellij un mesaj ales de noi
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
 }
